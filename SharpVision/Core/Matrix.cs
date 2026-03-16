d9792f89b9e897c1fdd95cf4208c22ca54da9b26
cenfraGit <cenfra_ma@hotmail.com>
2026-03-16 17:36:51 -0400

    feat: reallocate if needed (matrix)

--- SharpVision/Core/Matrix.cs
+++ SharpVision/Core/Matrix.cs
@@ -3,44 +3,71 @@
 // --------------------------------------------------------------------------------
 
 using System.Runtime.CompilerServices;
+using System.Buffers;
 
 namespace SharpVision;
 
-public class Matrix<T> where T : unmanaged
+public class Matrix<T> : IDisposable where T : unmanaged
 {
     // --------------------------------------------------------------------------------
     // fields and properties
     // --------------------------------------------------------------------------------
 
-    public int Rows { get; }
-    public int Columns { get; }
-    public int Channels { get; }
-    public int Stride { get; }
-    public int Length { get; }
+    public int Rows { get; private set; }
+    public int Columns { get; private set; }
+    public int Channels { get; private set; }
+    public int Stride { get; private set; }
+    public int Length { get; private set; }
 
-    public T[] Data { get; }
+    public T[] Data { get; private set; } = Array.Empty<T>();
 
     // --------------------------------------------------------------------------------
     // constructors
     // --------------------------------------------------------------------------------
 
+    public Matrix()
+    {
+    }
+
     public Matrix(int rows, int columns, int channels = 1)
     {
-        this.Rows = rows;
-        this.Columns = columns;
-        this.Channels = channels;
-        this.Stride = columns * channels;
-        this.Length = rows * columns * channels;
-        this.Data = new T[this.Length];
+        ReallocateIfNeeded(rows, columns, channels);
     }
 
     // --------------------------------------------------------------------------------
     // methods
     // --------------------------------------------------------------------------------
 
-    public Matrix<T> CopyStructureEmpty(int channels)
+    public void ReallocateIfNeeded(int rows, int columns, int channels)
     {
-        return new Matrix<T>(this.Rows, this.Columns, channels);
+        this.Rows = rows;
+        this.Columns = columns;
+        this.Channels = channels;
+        this.Stride = columns * channels;
+        this.Length = rows * columns * channels;
+
+        int requiredSize = rows * columns * channels;
+
+        // if we already have an array that's big enough, keep using
+        if (this.Data is not null && this.Data.Length >= requiredSize)
+            return;
+
+        // if current array too small, return to pool first
+        if (this.Data is not null && this.Data.Length > 0)
+            ArrayPool<T>.Shared.Return(this.Data);
+
+        // rent new array
+        this.Data = ArrayPool<T>.Shared.Rent(requiredSize);
+    }
+
+    public void Dispose()
+    {
+        if (this.Data.Length > 0)
+            ArrayPool<T>.Shared.Return(this.Data);
+        this.Data = Array.Empty<T>();
+        this.Rows = 0;
+        this.Columns = 0;
+        this.Channels = 0;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
