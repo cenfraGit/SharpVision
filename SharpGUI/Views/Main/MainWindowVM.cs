11ad8f41b030f38d43b021f3db077467e5865932
cenfraGit <cenfra_ma@hotmail.com>
2026-03-30 18:59:09 -0400

    feat: fetch methods via reflection to display in menu

--- SharpGUI/Views/Main/MainWindowVM.cs
+++ SharpGUI/Views/Main/MainWindowVM.cs
@@ -1,46 +1,83 @@
-﻿using MsBox.Avalonia;
-using MsBox.Avalonia.Enums;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
-using Dock.Model.Controls;
-using Dock.Model.Core;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using SharpGUI.Models.Layout;
-using SharpScript;
-using SharpGUI.Models.Editor;
 using CommunityToolkit.Mvvm.Messaging;
-using System.Text;
+using Dock.Model.Controls;
+using Dock.Model.Core;
+using SharpGUI.Models.Layout;
+using SharpGUI.Models.Messages;
+using SharpScript;
+using SharpVision;
 
 namespace SharpGUI.Views.Main;
 
 public partial class MainWindowVM : ObservableObject
 {
-        [ObservableProperty] IFactory? _factory;
-        [ObservableProperty] IRootDock? _layout;
+    public ObservableCollection<MenuItemViewModel> FunctionMenuItems { get; } = new();
+    [ObservableProperty] IFactory? _factory;
+    [ObservableProperty] IRootDock? _layout;
 
-        public MainWindowVM()
+    public MainWindowVM()
+    {
+        Factory = new DockFactory();
+        Layout = Factory?.CreateLayout();
+        if (Layout != null)
         {
-            Factory = new DockFactory();
-            Layout = Factory?.CreateLayout();
-            if (Layout != null)
+            Factory?.InitLayout(Layout);
+        }
+
+        // whenever we receive the requested script code, execute it
+        WeakReferenceMessenger.Default.Register<MessageExecuteCode>(this, async (r, m) => {
+
+            var environment = new SharpScriptEnvironment(m.ScriptCode);
+            environment.Run();
+            WeakReferenceMessenger.Default.Send(new MessageExecutionFinished(m.ScriptName, environment.Variables)); // send environment ref instead?
+        });
+
+        LoadFunctionsMenu();
+    }
+
+    [RelayCommand]
+    private async Task OnButtonRun()
+    {
+        // send request to run active script
+        WeakReferenceMessenger.Default.Send(new MessageRunActiveScript());
+    }
+
+    private void LoadFunctionsMenu()
+    {
+        var methods = typeof(Sharp).GetMethods(BindingFlags.NonPublic | BindingFlags.Static);
+
+        var discoveredFunctions = methods
+            .Select(m => new { Method = m, Attr = m.GetCustomAttribute<SharpFunctionAttribute>() })
+            .Where(x => x.Attr != null)
+            .ToList();
+
+        var groupedFunctions = discoveredFunctions.GroupBy(x => x.Attr!.Group);
+
+        foreach (var group in groupedFunctions)
+        {
+            var groupMenuItem = new MenuItemViewModel { Header = group.Key };
+
+            foreach (var func in group)
             {
-                Factory?.InitLayout(Layout);
+                var functionMenuItem = new MenuItemViewModel
+                {
+                    Header = func.Method.Name
+                };
+
+                groupMenuItem.Items.Add(functionMenuItem);
             }
-
-            // whenever we receive the requested script code, execute it
-            WeakReferenceMessenger.Default.Register<MessageExecuteCode>(this, async (r, m) => {
-
-                var runner = new SharpScriptRunner(m.Code);
-                runner.Run();
-
-                WeakReferenceMessenger.Default.Send(new MessageExecutionFinished("none", runner.Variables));
-            });
+            FunctionMenuItems.Add(groupMenuItem);
         }
+    }
+}
 
-        [RelayCommand]
-        private async Task OnButtonRun()
-        {
-            // send request to run active script
-            WeakReferenceMessenger.Default.Send(new MessageRunActiveScript());
-        }
+public class MenuItemViewModel
+{
+    public string Header { get; set; } = string.Empty;
+    public ObservableCollection<MenuItemViewModel> Items { get; } = new();
 }
