8027cce7accb7df727699c5e3d29e2719f21a210
cenfraGit <cenfra_ma@hotmail.com>
2026-03-30 18:55:48 -0400

    feat: update visitor with some features from new grammar

--- SharpScript/SharpScriptVisitor.cs
+++ SharpScript/SharpScriptVisitor.cs
@@ -1,84 +1,184 @@
+using System.Reflection;
+
 namespace SharpScript;
 
-public class SharpScriptVisitor : SharpScriptBaseVisitor<int>
+public class SharpScriptVisitor : SharpScriptBaseVisitor<object?>
 {
-    private readonly Dictionary<string, object?> _variables = new();
-    public Dictionary<string, object?> Variables => _variables;
+    private readonly Dictionary<string, object?> _variables;
+    private readonly Dictionary<string, MethodInfo> _registryNative;
+    private readonly Dictionary<string, SharpScriptParser.FunctionDeclarationStatContext> _registryUser;
 
-    public override int VisitProgram(SharpScriptParser.ProgramContext context)
+    // --------------------------------------------------------------------------------
+    // constructor
+    // --------------------------------------------------------------------------------
+
+    public SharpScriptVisitor(Dictionary<string, object?> variables,
+                              Dictionary<string, MethodInfo> registryNative,
+                              Dictionary<string, SharpScriptParser.FunctionDeclarationStatContext> registryUser)
     {
-        foreach (var decl in context.declaration())
-        {
-            Visit(decl);
-        }
-        return 0;
+        // use environment tables
+        _variables = variables;
+        _registryNative = registryNative;
+        _registryUser = registryUser;
     }
 
-    public override int VisitVarDecl(SharpScriptParser.VarDeclContext context)
+    public override object? VisitProgram(SharpScriptParser.ProgramContext context)
     {
-        string varName = context.ID().GetText();
-        int value = 0;
-        if (context.expr() != null)
-        {
-            value = Visit(context.expr());
-        }
-        _variables[varName] = value;
-        return value;
+        foreach (var statement in context.statement())
+            Visit(statement);
+        return null;
     }
 
-    public override int VisitAssignStmt(SharpScriptParser.AssignStmtContext context)
+    // --------------------------------------------------------------------------------
+    // statements
+    // --------------------------------------------------------------------------------
+
+    public override object? VisitAssignmentStat(SharpScriptParser.AssignmentStatContext context)
     {
-        string varName = context.assignment().ID().GetText();
-        int value = Visit(context.assignment().expr());
+        var rightSideValue = Visit(context.expr());
+        var variableNames = context.ID().Select(id => id.GetText()).ToArray();
 
-        if (!_variables.ContainsKey(varName))
-            throw new Exception($"Variable {varName} not defined.");
-
-        _variables[varName] = value;
-        return value;
-    }
-
-    public override int VisitIfStmt(SharpScriptParser.IfStmtContext context)
-    {
-        int condition = Visit(context.expr());
-
-        if (condition != 0)
+        if (variableNames.Length > 1)
         {
-            foreach (var decl in context.declaration())
+            if (rightSideValue is object[] resultsArray)
             {
-                Visit(decl);
+                if (variableNames.Length != resultsArray.Length)
+                    throw new Exception($"Cannot unpack {resultsArray.Length} values into {variableNames.Length} variables.");
+
+                for (int i = 0; i < variableNames.Length; i++)
+                {
+                    _variables[variableNames[i]] = resultsArray[i];
+                }
+            }
+            else
+            {
+                throw new Exception("Right side of assignment does not return multiple values.");
             }
         }
-        return 0;
+        else
+        {
+            if (rightSideValue is object[] { Length: 1 } singleResult)
+            {
+                _variables[variableNames[0]] = singleResult[0];
+            }
+            else
+            {
+                _variables[variableNames[0]] = rightSideValue;
+            }
+        }
+
+        return null;
     }
 
-    public override int VisitPrintStmt(SharpScriptParser.PrintStmtContext context)
+    // --------------------------------------------------------------------------------
+    // atom
+    // --------------------------------------------------------------------------------
+
+    public override object? VisitIntExpr(SharpScriptParser.IntExprContext context)
     {
-        int value = Visit(context.expr());
-        Console.WriteLine($"> {value}");
-        return value;
+        return int.Parse(context.INT().GetText());
     }
 
-    public override int VisitIntExpr(SharpScriptParser.IntExprContext context)
-        => int.Parse(context.INT().GetText());
-
-    public override int VisitIdExpr(SharpScriptParser.IdExprContext context)
+    public override object? VisitFloatExpr(SharpScriptParser.FloatExprContext context)
     {
-        string varName = context.ID().GetText();
-        return (int)_variables.GetValueOrDefault(varName, 0);
+        return float.Parse(context.FLOAT().GetText());
     }
 
-    public override int VisitAddSubExpr(SharpScriptParser.AddSubExprContext context)
+    public override object? VisitStringExpr(SharpScriptParser.StringExprContext context)
     {
-        int left = Visit(context.expr(0));
-        int right = Visit(context.expr(1));
-        return context.op.Text == "+" ? left + right : left - right;
+        var rawString = context.STRING().GetText();
+        return rawString.Substring(1, rawString.Length - 2);
     }
 
-    public override int VisitMulDivExpr(SharpScriptParser.MulDivExprContext context)
+    public override object? VisitIdExpr(SharpScriptParser.IdExprContext context)
     {
-        int left = Visit(context.expr(0));
-        int right = Visit(context.expr(1));
-        return context.op.Text == "*" ? left * right : left / right;
+        string id = context.ID().GetText();
+        return _variables[id];
+    }
+
+    // --------------------------------------------------------------------------------
+    // arithmetic
+    // --------------------------------------------------------------------------------
+
+    public override object? VisitPlusMinusExpr(SharpScriptParser.PlusMinusExprContext context)
+    {
+        dynamic? left = Visit(context.expr(0));
+        dynamic? right = Visit(context.expr(1));
+        return context.PLUS() != null ? left + right : left - right;
+    }
+
+    public override object? VisitMulDivExpr(SharpScriptParser.MulDivExprContext context)
+    {
+        dynamic? left = Visit(context.expr(0));
+        dynamic? right = Visit(context.expr(1));
+        return context.MUL() != null ? left * right : left / right;
+    }
+
+    // --------------------------------------------------------------------------------
+    // functions
+    // --------------------------------------------------------------------------------
+
+    public override object? VisitFunctionCallStat(SharpScriptParser.FunctionCallStatContext context)
+    {
+        string functionName = context.ID().GetText();
+
+        var argsContext = context.expr();
+        object?[] providedArgs = argsContext != null ? [.. argsContext.Select(e => Visit(e))] : [];
+
+        if (_registryNative.TryGetValue(functionName, out MethodInfo? nativeMethod))
+        {
+            var expectedParameters = nativeMethod.GetParameters();
+
+            if (expectedParameters.Length != providedArgs.Length)
+            {
+                throw new Exception($"Function \"{functionName}\" expects {expectedParameters.Length} arguments, but got {providedArgs.Length}.");
+            }
+
+            object?[] coercedArgs = new object?[providedArgs.Length];
+
+            for (int i = 0; i < expectedParameters.Length; i++)
+            {
+                Type expectedType = expectedParameters[i].ParameterType;
+                object? providedValue = providedArgs[i];
+
+                if (providedValue == null)
+                {
+                    coercedArgs[i] = null;
+                    continue;
+                }
+
+                if (expectedType.IsAssignableFrom(providedValue.GetType()))
+                {
+                    coercedArgs[i] = providedValue;
+                }
+                else
+                {
+                    try
+                    {
+                        coercedArgs[i] = Convert.ChangeType(providedValue, expectedType);
+                    }
+                    catch (InvalidCastException)
+                    {
+                        throw new Exception($"Cannot convert argument {i + 1} (\"{providedValue}\") from {providedValue.GetType().Name} to {expectedType.Name} in function \"{functionName}\".");
+                    }
+                }
+            }
+
+            try
+            {
+                return nativeMethod.Invoke(null, coercedArgs);
+            }
+            catch (TargetInvocationException ex)
+            {
+                throw ex.InnerException ?? ex;
+            }
+        }
+
+        if (_registryUser.TryGetValue(functionName, out var userFuncContext))
+        {
+            throw new NotImplementedException("User-defined function");
+        }
+
+        throw new Exception($"Unknown function: \"{functionName}\"");
     }
 }
